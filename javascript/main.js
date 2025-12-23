fd754c1aada86bc01d1003532baf653765cbda24
vzrno <vjeko.zrno.badboy@gmail.com>
2025-12-23 10:11:58 +0100

    2 verzija

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * Glavna ulazna točka
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  initNavigator();
+  initSmoothScroll();
+  initContractForm();
+  initUIEffiect();
+});
+
+/** * 1. Aktivna Navigacija
+ * Označava aktivnu stranicu u <nav>
+ */
