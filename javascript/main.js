c97380c049aa7932c47c339fdff6571da7191ffd
vzrno <vjeko.zrno.badboy@gmail.com>
2025-12-23 10:21:41 +0100

    2 verzija

--- javascript/main.js
+++ javascript/main.js
@@ -13,3 +13,23 @@ document.addEventListener("DOMContentLoaded", () => {
 /** * 1. Aktivna Navigacija
  * Označava aktivnu stranicu u <nav>
  */
+function initNavigator() {
+  const navLinks = document.querySelectorAll("nav a");
+  const currentPage = window.location.pathname.split("/").pop();
+
+  navLinks.forEach((link) => {
+    const linkPage = link.getAttribute("href");
+
+    if (
+      linkPage === currentPage ||
+      (currentPage === "" && linkPage === "index.html")
+    ) {
+      link.style.color = "#ffcc00";
+      link.style.fontWeight = "700";
+    }
+  });
+}
+
+/**
+ * 2. Glatko scrolanje za linkove
+ */
