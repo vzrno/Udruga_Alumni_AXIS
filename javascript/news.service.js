99972edd33d2415e3db1855611325c88b757fadf
vzrno <vjeko.zrno.badboy@gmail.com>
2025-12-30 20:24:22 +0100

    11 verzija

--- javascript/news.service.js
+++ javascript/news.service.js
@@ -1,7 +1,8 @@
 // dohvat novosti (JSON / API)
 
 const NewsService = (() => {
-    const DATA_URL = "data/novosti.json"; // Promijeniti samo DATA_URL za CMS / API migraciju
+    const DATA_URL_1 = "data/novosti.json"; // Promijeniti samo DATA_URL za CMS / API migraciju
+    const DATA_URL_2 = "data/nas.rad.json"; // Promijeniti samo DATA_URL za CMS / API migraciju
 
     async function getAll() {
         const res = await fetch(DATA_URL);
